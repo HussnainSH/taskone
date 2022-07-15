0522c382763dff103d4ca4d0375960782b6a3a9a
Hassaan Aslam <hassaan.aslam@intalgeo.com>
2022-07-15 17:18:11 +0500

    Initial commit

---
+++ task2.py
@@ -0,0 +1,29 @@
+started = False
+stopped = False
+
+while True:
+    car = input(f'>').lower()
+    if car == 'start':
+        if started:
+            print(f'Car is already started!!')
+        else:
+            started = True
+            stopped = False
+            print(f'Car Started... Ready to go!')
+    elif car == 'stop':
+        if stopped:
+            print(f'Car is already Stopped!!')
+        else:
+            stopped = True
+            started = False
+            print(f'Car Stopped.')
+    elif car == 'help':
+        print("""
+start - to start the car
+stop - to stop the car
+quit - to exit
+        """)
+    elif car == 'quit':
+        break
+    else:
+        print("I don't understand that...")
